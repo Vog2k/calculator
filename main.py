7ecdfa11a64220d3885558cb2a2ab7347c6167ce
Vog2k <99256921+Vog2k@users.noreply.github.com>
2022-05-10 10:09:19 +1200

    Edited my last upload
    Created variables again and also got to the stage where i could calculate 10+20=30 worked perfectly but after adding a decimal point it crashed i then had to delete the 'int' in front of my number variables. by adding float in place of int the equation came 2out perfect. next step is to add the symbols like subtract addition and more.

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-#These are my variables/that consist of words i will use to create funcations#
+'''When ready insert the number - eventually
+
+Below this line is all the juicy stuff involving the code to multiply and subtract ect .... '''
+
+# String of words that display as the programme run's
+
+#Writing this line below made me realise that arrays exisit and i should use them instead of trying to write new vars for each symbol
+array = ("Addition", "Subtraction", "Multiply", "Divide")
+
+Addition = "+"
+# This line adds to the equation
+Subtraction = "-"
+# This line subtracts from the equation
+Divide = "÷"
+# This line divides two numbers also I had to Google how to write the divide sign and its (alt + 0247)
+Multiply = "×"
+# alt + 0215 to get the multiply sign. This multiplies the current number
 
 
-equals = "equals"
-#This line adds to the equation
-plus = '+'
-#This multiplies the current number
-times = "x"
-#This line divides two numbers also i had google how to write the divide sign and its (alt + 0247)
-divide = '÷'
-#Num_input is the equation
-num_input = "Equation"
+first_number = input("first number :")
+# first number is the start of this equation
 
+Symbol = input(array)
+#'-'='+'='×'='÷'='array'
+# This should bring up an array of different symbols for the user to chose from
 
+second_number = input("second number :")
+# Second number is the start of this equation
+''' I have discovered that putting int in front of first and second makes this function know it's a plus or minus ect.
+doing this made me run into errors with decimal points we just learned about float today so i will try replace int for float'''
 
-#String of words that display as the programe run's
-Input = input("Please enter an equation  ")
-num_input = input("")
-print("Plus")
-print("Minus")
-print("Times")
-print("Divide")
-("= ")
+result = float(first_number) + type(array) + float(second_number)
+
+'''running into an error i had to write a str in front of result because it wouldn't recognise the float'''
+print("result" + str(result))
+
+'''print("#1 Plus")
+print("#2 Minus")
+print("#3 Times")
+print("#4 Divide")'''
 
 '''
 Below this line is my little cheat sheet.
