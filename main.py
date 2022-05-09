f25ee7125c1f88d91617d30cd5b47ec7530ef598
Tim <99256921+Vog2k@users.noreply.github.com>
2022-05-09 18:56:35 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#These are my variables/that consist of words i will use to create funcations#
+
+
+equals = "equals"
+#This line adds to the equation
+plus = '+'
+#This multiplies the current number
+times = "x"
+#This line divides two numbers also i had google how to write the divide sign and its (alt + 0247)
+divide = '÷'
+#Num_input is the equation
+num_input = "Equation"
+
+
+
+#String of words that display as the programe run's
+Input = input("Please enter an equation  ")
+num_input = input("")
+print("Plus")
+print("Minus")
+print("Times")
+print("Divide")
+("= ")
+
+'''
+Below this line is my little cheat sheet.
+
+(Int) or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+(Ranges) returns a sequence of numbers starting from zero and increment by 1 by default and stops before the given number.
+(Variable) symbolic name that is a reference or pointer to an object
+(Array) collection of common type of data structures having elements with same data type
+
+unfinished code
+Input = input("Please enter an equation  ")
+Equals = input("")
+num_input = ('0') - int(Equals)
+float('inf')
+
+Numbers, Strings, Boolean'''
