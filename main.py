daf7fe53233e9e74c21866cddb07fe1156dd3598
Tim <99256921+Vog2k@users.noreply.github.com>
2022-05-10 12:48:21 +1200

    i worked another 2 hours and came to an end i understood alot of what is happening in this code

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 '''When ready insert the number - eventually
 
-Below this line is all the juicy stuff involving the code to multiply and subtract ect .... '''
+Below this line is all the juicy stuff involving the code to multiply and subtract ect
+Writing this line below made me realise that arrays exisit and i should use them instead of trying to write new vars for each symbol'''
+while True:
+    array = "Addition", "Subtraction", "Multiply", "Divide"
+    Addition = "+"
+    # This line adds to the equation
+    Subtraction = "-"
+    # This line subtracts from the equation
+    Divide = "/"
+    # This line divides two numbers also I had to Google how to write the divide sign and its (alt + 0247)
+    Multiply = "*"
+    # alt + 0215 to get the multiply sign. This multiplies the current number
 
-# String of words that display as the programme run's
+    first_number = input("first number :")
+    # first number is the start of this equation
 
-#Writing this line below made me realise that arrays exisit and i should use them instead of trying to write new vars for each symbol
-array = ("Addition", "Subtraction", "Multiply", "Divide")
+    Symbol = input(array)
+    # This should bring up an array of different symbols for the user to chose from
 
-Addition = "+"
-# This line adds to the equation
-Subtraction = "-"
-# This line subtracts from the equation
-Divide = "÷"
-# This line divides two numbers also I had to Google how to write the divide sign and its (alt + 0247)
-Multiply = "×"
-# alt + 0215 to get the multiply sign. This multiplies the current number
+    second_number = input("second number :")
+    # Second number is the start of this equation
+    ''' I have discovered that putting int in front of first and second makes this function know it's a plus or minus ect.
+    doing this made me run into errors with decimal points we just learned about float today so i will try replace int for float'''
 
+    # Original code: result = float(first_number) + float(second_number)
+    '''This line below took me at least two hours to understand,
+    but as i add an if statement and the variable this sets up my demand telling the system if '==' this is equal to the symbol from
+    the array i created "+" then tell the system to print the first number and plus the second number aswell giving the results for your equation
+    then pretty much from there i just copy and past the same code for the other symbols'''
+    if first_number == "Q" or first_number == "q":
+        break
 
-first_number = input("first number :")
-# first number is the start of this equation
+    if Symbol == "+":
+        print(first_number, "+", second_number, "=", (int(first_number) + int(second_number)))
+    elif Symbol == "*":
+        print(first_number, "*", second_number, "=", (int(first_number) * int(second_number)))
+    elif Symbol == "-":
+        print(first_number, "-", second_number, "=", (int(first_number) - int(second_number)))
+    elif Symbol == "/":
+        print(first_number, "/", second_number, "=", (float(first_number) / float(second_number)))
+    print("Press Q or q to exit")
 
-Symbol = input(array)
-#'-'='+'='×'='÷'='array'
-# This should bring up an array of different symbols for the user to chose from
+print("Application ended thank you")
 
-second_number = input("second number :")
-# Second number is the start of this equation
-''' I have discovered that putting int in front of first and second makes this function know it's a plus or minus ect.
-doing this made me run into errors with decimal points we just learned about float today so i will try replace int for float'''
+''' I kept running into a wall while doing these elif statements because i didn't set up float in front of the
+numbers that will equal to a decimal or have int in front of the whole numbers'''
 
-result = float(first_number) + type(array) + float(second_number)
-
-'''running into an error i had to write a str in front of result because it wouldn't recognise the float'''
-print("result" + str(result))
+# print("result" + str())
 
 '''print("#1 Plus")
 print("#2 Minus")
@@ -53,4 +68,18 @@ Equals = input("")
 num_input = ('0') - int(Equals)
 float('inf')
 
-Numbers, Strings, Boolean'''
+Numbers, Strings, Boolean
+
+Comparison operators
+< less than
+< less than or equal to
+> grater than
+>= grater than or equal to
+== equal
+!= not equal 
+
+
+
+
+
+'''
